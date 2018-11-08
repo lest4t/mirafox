6f776a9bec8a335d2186b93e22f1903fa1c4caf7
lest4t <itlest4t@gmail.com>
2018-11-08 15:57:19 +0500

    added WC's commands data

--- data.php
+++ data.php
@@ -4,3 +4,368 @@
  * Time: 3:11 PM
  * Author: lest4t
  */
+
+/**
+ * Данные выступления сборных на ЧМ
+ * name - название команды
+ * games - количество игр
+ * win - количество побед
+ * defeat - количество поражений
+ * draw - количество матчей в ничью
+ * scored - количество забитых голов у команды
+ * skipped - количество пропущенных голов у команды
+ */
+$data = array(
+	0  => array(
+		'name'   => 'Австрия',
+		'games'  => 29,
+		'win'    => 12,
+		'defeat' => 13,
+		'draw'   => 4,
+		'goals'  => array(
+			'scored'  => 43,
+			'skipped' => 47,
+		),
+	),
+	1  => array(
+		'name'   => 'Англия',
+		'games'  => 62,
+		'win'    => 26,
+		'defeat' => 16,
+		'draw'   => 20,
+		'goals'  => array(
+			'scored'  => 79,
+			'skipped' => 56,
+		),
+	),
+	2  => array(
+		'name'   => 'Аргентина',
+		'games'  => 77,
+		'win'    => 42,
+		'defeat' => 21,
+		'draw'   => 14,
+		'goals'  => array(
+			'scored'  => 131,
+			'skipped' => 84,
+		),
+	),
+	3  => array(
+		'name'   => 'Бельгия',
+		'games'  => 41,
+		'win'    => 14,
+		'defeat' => 18,
+		'draw'   => 9,
+		'goals'  => array(
+			'scored'  => 52,
+			'skipped' => 66,
+		),
+	),
+	4  => array(
+		'name'   => 'Болгария',
+		'games'  => 26,
+		'win'    => 3,
+		'defeat' => 15,
+		'draw'   => 8,
+		'goals'  => array(
+			'scored'  => 22,
+			'skipped' => 53,
+		),
+	),
+	5  => array(
+		'name'   => 'Бразилия',
+		'games'  => 104,
+		'win'    => 70,
+		'defeat' => 17,
+		'draw'   => 17,
+		'goals'  => array(
+			'scored'  => 221,
+			'skipped' => 102,
+		),
+	),
+	6  => array(
+		'name'   => 'Венгрия',
+		'games'  => 32,
+		'win'    => 15,
+		'defeat' => 14,
+		'draw'   => 3,
+		'goals'  => array(
+			'scored'  => 87,
+			'skipped' => 57,
+		),
+	),
+	7  => array(
+		'name'   => 'Германия/ФРГ',
+		'games'  => 106,
+		'win'    => 66,
+		'defeat' => 20,
+		'draw'   => 20,
+		'goals'  => array(
+			'scored'  => 224,
+			'skipped' => 121,
+		),
+	),
+	8  => array(
+		'name'   => 'Голландия',
+		'games'  => 50,
+		'win'    => 27,
+		'defeat' => 11,
+		'draw'   => 12,
+		'goals'  => array(
+			'scored'  => 86,
+			'skipped' => 48,
+		),
+	),
+	9  => array(
+		'name'   => 'Дания',
+		'games'  => 16,
+		'win'    => 8,
+		'defeat' => 6,
+		'draw'   => 2,
+		'goals'  => array(
+			'scored'  => 27,
+			'skipped' => 24,
+		),
+	),
+	10 => array(
+		'name'   => 'Испания',
+		'games'  => 59,
+		'win'    => 29,
+		'defeat' => 12,
+		'draw'   => 18,
+		'goals'  => array(
+			'scored'  => 92,
+			'skipped' => 66,
+		),
+	),
+	11 => array(
+		'name'   => 'Италия',
+		'games'  => 83,
+		'win'    => 45,
+		'defeat' => 17,
+		'draw'   => 21,
+		'goals'  => array(
+			'scored'  => 128,
+			'skipped' => 77,
+		),
+	),
+	12 => array(
+		'name'   => 'Камерун',
+		'games'  => 23,
+		'win'    => 4,
+		'defeat' => 12,
+		'draw'   => 7,
+		'goals'  => array(
+			'scored'  => 18,
+			'skipped' => 43,
+		),
+	),
+	13 => array(
+		'name'   => 'Колумбия',
+		'games'  => 18,
+		'win'    => 7,
+		'defeat' => 9,
+		'draw'   => 2,
+		'goals'  => array(
+			'scored'  => 26,
+			'skipped' => 27,
+		),
+	),
+	14 => array(
+		'name'   => 'Коста-Рика',
+		'games'  => 15,
+		'win'    => 5,
+		'defeat' => 6,
+		'draw'   => 4,
+		'goals'  => array(
+			'scored'  => 17,
+			'skipped' => 23,
+		),
+	),
+	15 => array(
+		'name'   => 'Мексика',
+		'games'  => 53,
+		'win'    => 14,
+		'defeat' => 25,
+		'draw'   => 14,
+		'goals'  => array(
+			'scored'  => 57,
+			'skipped' => 92,
+		),
+	),
+	16 => array(
+		'name'   => 'Парагвай',
+		'games'  => 27,
+		'win'    => 7,
+		'defeat' => 10,
+		'draw'   => 10,
+		'goals'  => array(
+			'scored'  => 30,
+			'skipped' => 38,
+		),
+	),
+	17 => array(
+		'name'   => 'Польша',
+		'games'  => 31,
+		'win'    => 15,
+		'defeat' => 11,
+		'draw'   => 5,
+		'goals'  => array(
+			'scored'  => 44,
+			'skipped' => 40,
+		),
+	),
+	18 => array(
+		'name'   => 'Португалия',
+		'games'  => 26,
+		'win'    => 13,
+		'defeat' => 9,
+		'draw'   => 4,
+		'goals'  => array(
+			'scored'  => 43,
+			'skipped' => 29,
+		),
+	),
+	19 => array(
+		'name'   => 'Россия/СССР',
+		'games'  => 40,
+		'win'    => 17,
+		'defeat' => 15,
+		'draw'   => 8,
+		'goals'  => array(
+			'scored'  => 66,
+			'skipped' => 47,
+		),
+	),
+	20 => array(
+		'name'   => 'Румыния',
+		'games'  => 21,
+		'win'    => 8,
+		'defeat' => 8,
+		'draw'   => 5,
+		'goals'  => array(
+			'scored'  => 30,
+			'skipped' => 32,
+		),
+	),
+	21 => array(
+		'name'   => 'США',
+		'games'  => 33,
+		'win'    => 8,
+		'defeat' => 19,
+		'draw'   => 6,
+		'goals'  => array(
+			'scored'  => 37,
+			'skipped' => 62,
+		),
+	),
+	22 => array(
+		'name'   => 'Сербия/Югославия',
+		'games'  => 43,
+		'win'    => 17,
+		'defeat' => 18,
+		'draw'   => 8,
+		'goals'  => array(
+			'scored'  => 64,
+			'skipped' => 59,
+		),
+	),
+	23 => array(
+		'name'   => 'Уругвай',
+		'games'  => 51,
+		'win'    => 20,
+		'defeat' => 19,
+		'draw'   => 12,
+		'goals'  => array(
+			'scored'  => 80,
+			'skipped' => 71,
+		),
+	),
+	24 => array(
+		'name'   => 'Франция',
+		'games'  => 59,
+		'win'    => 28,
+		'defeat' => 19,
+		'draw'   => 12,
+		'goals'  => array(
+			'scored'  => 106,
+			'skipped' => 71,
+		),
+	),
+	25 => array(
+		'name'   => 'Хорватия',
+		'games'  => 16,
+		'win'    => 7,
+		'defeat' => 7,
+		'draw'   => 2,
+		'goals'  => array(
+			'scored'  => 21,
+			'skipped' => 17,
+		),
+	),
+	26 => array(
+		'name'   => 'Чехия/Чехословакия',
+		'games'  => 33,
+		'win'    => 12,
+		'defeat' => 16,
+		'draw'   => 5,
+		'goals'  => array(
+			'scored'  => 47,
+			'skipped' => 49,
+		),
+	),
+	27 => array(
+		'name'   => 'Чили',
+		'games'  => 33,
+		'win'    => 11,
+		'defeat' => 15,
+		'draw'   => 7,
+		'goals'  => array(
+			'scored'  => 40,
+			'skipped' => 49,
+		),
+	),
+	28 => array(
+		'name'   => 'Швейцария',
+		'games'  => 33,
+		'win'    => 11,
+		'defeat' => 16,
+		'draw'   => 6,
+		'goals'  => array(
+			'scored'  => 45,
+			'skipped' => 59,
+		),
+	),
+	29 => array(
+		'name'   => 'Швеция',
+		'games'  => 46,
+		'win'    => 16,
+		'defeat' => 13,
+		'draw'   => 17,
+		'goals'  => array(
+			'scored'  => 74,
+			'skipped' => 69,
+		),
+	),
+	30 => array(
+		'name'   => 'Шотландия',
+		'games'  => 23,
+		'win'    => 4,
+		'defeat' => 12,
+		'draw'   => 7,
+		'goals'  => array(
+			'scored'  => 25,
+			'skipped' => 41,
+		),
+	),
+	31 => array(
+		'name'   => 'Южная Корея',
+		'games'  => 31,
+		'win'    => 5,
+		'defeat' => 17,
+		'draw'   => 9,
+		'goals'  => array(
+			'scored'  => 31,
+			'skipped' => 67,
+		),
+	),
+);
